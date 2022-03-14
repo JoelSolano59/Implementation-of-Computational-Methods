53745084955d6a4f9b9ce0df7a6675fc0ae54473
Joel Solano <joel.solano59@gmail.com>
2022-03-14 12:58:32 -0600

    Comments added
    Added some comments for better understanding.

--- Act_3.2/Act3_2.cpp
+++ Act_3.2/Act3_2.cpp
@@ -1,3 +1,4 @@
+// Inluimos las librerias necesarias.
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
@@ -5,471 +6,244 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> vec;
-std::string token = "";
-int qg = 0; 
-//std::string tv, tr, ti = "";
-//int qv, qr, qi = 0;
+// Declaramos nuestras variables globales.
+std::vector<std::string> vec;	// Vector donde guardaremos nuestra salida.
+std::string token = "";			// String donde guardaremos el token.
+int qg = 0; 					// Entero que estara al tanto de los estados del DFA.
 
+// Funcion para agregar el token y su tipo al vector.
 void setToken(std::string t, std::string type){
 	vec.push_back(t);
 	vec.push_back(type);
 }
 
+// Funcion para cambiar el estado de qg y agregar el caracter actual al token.
 void setState(int q, char const t){
-	// switch (dfa){
-	// case 'v':
-	// 	qv = q;
-	// 	tv = tv + t;
-	// 	break;
-	// case 'r':
-	// 	qr = q;
-	// 	tr = tr + t;
-	// 	break;
-	// case 'i':
-	// 	qi = q;
-	// 	ti = ti + t;
-	// 	break;
-	// default:
-	// 	break;
-	// }
 	qg = q;
 	token = token + t;
 }
 
+// Funcion para asignar el valor de qg a 0 y el token a "".
 void resetState(){
-	// switch (dfa){
-	// case 'v':
-	// 	qv = 0;
-	// 	tv = "";
-	// 	break;
-	// case 'r':
-	// 	qr = 0;
-	// 	tr = "";
-	// 	break;
-	// case 'i':
-	// 	qi = 0;
-	// 	ti = "";
-	// 	break;
-	// default:
-	// 	break;
-	// }
 	qg = 0;
 	token = "";
 }
 
+// Funcion para asignar el tipo de operador segun el caracter.
 void setOperation(char const s){
-	switch (s){
-	case '=':
+	switch (s){		// Switch para asignar el tipo de operador en base al parametro.
+	case '=':		// Si es asignacion.
 		setToken("=", "Asignacion");
 		break;
-	case '+':
+	case '+':		// Si es suma.
 		setToken("+", "Suma");
 		break;
-	case '-':
+	case '-':		// Si es resta.
 		setToken("-", "Resta");
 		break;
-	case '*':
+	case '*':		// Si es multiplicacion.
 		setToken("*", "Multiplicacion");
 		break;
-	case '/':
+	case '/':		// Si es division.
 		setToken("/", "Division");
 		break;
-	case '^':
+	case '^':		// Si es exponente.
 		setToken("^", "Exponente");
 		break;
-	case '(':
+	case '(':		// Si es simbolo especial.
 		setToken("(", "Parentesis que abre");
 		break;
-	case ')':
+	case ')':		// Si es simbolo especial.
 		setToken(")", "Parentesis que cierra");
 		break;
-	default:
+	default:		// Ninguna de las anteriores.
 		break;
 	}
 }
-/*
-void isVariable(std::string str){
-	for (char const &s : str){
-		switch (qv){
-		case 0:
-			if (isalpha(s) && (islower(s) || isupper(s))){
-				setState('v', 1, s);
-			} else {
-				resetState('v');
-			}
-			break;
-		case 1:
-			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '/' || s == '^' || s == '(' || s == ')'){
-				setToken(tv, "Variable");
-				setOperation(s);
-				resetState('v');
-			} else if (isalpha(s) && (islower(s) || isupper(s))){
-				setState('v', 1, s);
-			} else if (isdigit(s)){
-				setState('v', 1, s);
-			} else if (s == '_'){
-				setState('v', 1, s);
-			} else if (s == ' '){
-				setToken(tv, "Variable");
-				resetState('v');
-			} else {
-				resetState('v');
-			}
-			break;
-		default:
-			break;
-		}
-	}
-	if (tv.length() > 0 && (qv == 1 || qv == 2 || qv == 3 || qv == 4)){
-		setToken(tv, "Variable");
-	}
-}
 
-void isInteger(std::string str){
-	for (char const &s : str){
-		switch (qi){
-		case 0:
-			if (s == '+' || s == '-'){
-				setState('i', 1, s);
-			} else if (isdigit(s)){
-				setState('i', 2, s);
-			} else {
-				resetState('i');
-			}
-			break;
-		case 1:
-			if (isdigit(s)){
-				setState('i', 2, s);
-			} else {
-				resetState('i');
-			}
-			break;
-		case 2:
-			if (s == '.') {
-				resetState('i');
-			} else if (isdigit(s)){
-				setState('i', 2, s);
-			} else if (s == '=' || s == '+' || s == '-' || s == '*' || s == '/' || s == '^' || s == '(' || s == ')'){
-				setToken(ti, "Entero");
-				setOperation(s);
-				resetState('i');
-			} else if (s == ' '){
-				setToken(ti, "Entero");
-				resetState('i');
-			} else {
-				resetState('i');
-			}
-			break;
-		default:
-			break;
-		}
-	}
-	if (ti.length() > 0 && (qi == 2)){
-		setToken(ti, "Entero");
-	}
-}
-
-void isReal(std::string str){
-	for (char const &s : str){
-		switch (qr){
-		case 0:
-			if (isdigit(s)){
-				setState('r', 1, s);
-			} else if (s == '+' || s == '-'){
-				setState('r', 2, s);
-			} else if (s == '.'){
-				setState('r', 3, s);
-			} else {
-				resetState('r');
-			}
-			break;
-		case 1:
-			if (isdigit(s)){
-				setState('r', 1, s);
-			} else if (s == '.'){
-				setState('r', 3, s);
-			} else {
-				resetState('r');
-			}
-			break;
-		case 2:
-			if (isdigit(s)){
-				setState('r', 1, s);
-			} else if (s == '.'){
-				setState('r', 3, s);
-			} else {
-				resetState('r');
-			}
-			break;
-		case 3:
-			if (isdigit(s)){
-				setState('r', 4, s);
-			} else if (s == ' '){
-				setToken(tr, "Real");
-				resetState('r');
-			} else {
-				resetState('r');
-			}
-			break;
-		case 4:
-			if (isdigit(s)){
-				setState('r', 4, s);
-			} else if (s == 'e' || s == 'E'){
-				setState('r', 5, s);
-			} else if (s == ' '){
-				setToken(tr, "Real");
-				resetState('r');
-			} else {
-				resetState('r');
-			}
-			break;
-		case 5:
-			if (isdigit(s)){
-				setState('r', 7, s);
-			} else if (s == '+' || s == '-'){
-				setState('r', 6, s);
-			} else {
-				resetState('r');
-			}
-			break;
-		case 6:
-			if (isdigit(s)){
-				setState('r', 7, s);
-			} else {
-				resetState('r');
-			}
-		case 7:
-			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '/' || s == '^' || s == '(' || s == ')'){
-				setToken(tr, "Real");
-				setOperation(s);
-				resetState('r');
-			} else if (s == ' '){
-				setToken(tr, "Real");
-				resetState('r');
-			} else {
-				resetState('r');
-			}
-			break;
-		default:
-			break;
-		}
-	}
-	if (tr.length() > 0 && (qr == 4 || qr == 7)){
-		setToken(tr, "Real");
-	}
-}
-
-bool isComment(std::string str){
-	std::string prefix1 = "/";
-	std::string prefix2 = "//";
-	for (char const &s : str){
-		if (s == '/'){
-			if (token == ""){
-				token = token + s;
-			} else if (token == prefix1){
-				token = token + s;
-			}
-		} else if (prefix2 == token.substr(0, prefix2.size())){
-			token = token + s;
-		}
-	}
-	if (prefix2 == token.substr(0, prefix2.size()) && token.length() >= 2){
-		setToken(token, "Comentario");
-		return true;
-	} else {
-		token = "";
-		return false;
-	}
-}
-*/
+// Funcion de DFA, recibe la linea completa como parametro.
 void DFA(std::string str){
-	bool error = false;
-	for (char const &s : str){
-		switch (qg){
-		case 0:
-			if ((s == '=' || s == '+' || s == '*' || s == '^' || s == '(' || s == ')') && error == false){
+	bool error = false;				// Booleano para detectar errores.
+	for (char const &s : str){		// Ciclo for para cada caracter de la linea de texto.
+		switch (qg){				// Switch para cada estado de nuestro DFA.
+		case 0:						// En caso 0, tratamos de identificar primero que clase de token es.
+			if ((s == '=' || s == '+' || s == '*' || s == '^' || s == '(' || s == ')') && error == false){	// Si es un operador.
 				setOperation(s);
 				resetState();
-			} else if (s == '/' && error == false){
+			} else if (s == '/' && error == false){		// Si es una division o comentario.
 				setState(9, s);
-			} else if (s == '-' && error == false){
+			} else if (s == '-' && error == false){		// Si es una resta, real o entero.
 				setState(2, s);
-			} else if (isalpha(s) && (islower(s) || isupper(s)) && error == false){
+			} else if (isalpha(s) && (islower(s) || isupper(s)) && error == false){		// Si es una variable.
 				setState(1, s);
-			} else if (isdigit(s) && error == false){
+			} else if (isdigit(s) && error == false){	// Si es un entero o real.
 				setState(3, s);
-			} else if (s == ' ' && error == false){
+			} else if (s == ' ' && error == false){		// Los espacios no se asignan pero ayudan a determinar cuando un token a finalizado.
 				resetState();
-			} else {
+			} else {									// Si no se cumple ninguna condicion if, detectamos que es un error y no nos permite agregar nada al token hasta finalizar la linea de texto.
 				error = true;
 				resetState();
 			}
 			break;
-		case 1:
-			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '/' || s == '^' || s == '(' || s == ')'){
-				setToken(token, "Variable");
-				setOperation(s);
+		case 1:		// En caso 1, el token es posible a ser una variable.
+			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '/' || s == '^' || s == '(' || s == ')'){	// Si es un operador.
+				setToken(token, "Variable");	
+				setOperation(s);				
 				resetState();
-			} else if (s == '/'){
+			} else if (s == '/'){	// Si es division o comentario.
 				setToken(token, "Variable");
 				setState(9, s);
-			} else if ((isalpha(s) && (islower(s) || isupper(s))) || isdigit(s) || s == '_'){
+			} else if ((isalpha(s) && (islower(s) || isupper(s))) || isdigit(s) || s == '_'){	// Si es una variable.
 				setState(1, s);
-			} else if (s == ' '){
+			} else if (s == ' '){	// Fin del token.
 				setToken(token, "Variable");
 				resetState();
-			} else {
+			} else {				// Ninguna de las anteriores.
 				resetState();
 			}
 			break;
-		case 2:
-			if (isdigit(s)){
+		case 2:		// En el caso 2, el token puede ser un entero o real negativo.
+			if (isdigit(s)){		// Si es un digito.
 				setState(3, s);
-			} else if (s == '.'){
+			} else if (s == '.'){	// Si es real.
 				setState(4, s);
-			} else if (s == ' ' && token == "-"){
+			} else if (s == ' ' && token == "-"){	// Fin del token, pero teniendo en cuenta que se ingreso una resta previamente, por lo que asignamos el operador.
 				setOperation('-');
 				resetState();
-			} else {
+			} else {				// Ninguna de las anteriores.
 				resetState();
 			}
 			break;
-		case 3:
-			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '^' || s == '(' || s == ')'){
+		case 3:		// En el caso 3, sabemos que el token a evaluar es un entero o real.
+			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '^' || s == '(' || s == ')'){	// Si es un operador.
 				setToken(token, "Entero");
 				setOperation(s);
 				resetState();
-			} else if (s == '/'){
+			} else if (s == '/'){		// Si es division o comentario.
 				setState(9, s);
-			} else if (isdigit(s)){
+			} else if (isdigit(s)){		// Si es un entero.
 				setState(3, s);
-			} else if (s == '.') {
+			} else if (s == '.') {		// Si es un real.
 				setState(4, s);
-			} else {
+			} else {					// Ninguna de las anteriores.
 				resetState();
 			}
 			break;
-		case 4:
-			if (isdigit(s)){
+		case 4:		// En el caso 4, sabemos que el token solo puede ser un real.
+			if (isdigit(s)){	// Si es un real.
 				setState(5, s);
-			} else {
+			} else {			// En este estado, solo puede seguir siendo un real si se ingresa un digito, de no ser asi, se reinicia el token y el estado.
 				resetState();
 			}
 			break;
-		case 5:
-			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '^' || s == '(' || s == ')'){
+		case 5:		// En el caso 5, sabemos que es real, positivo o negativo con notacion decimal, pero puede concluir ahi o seguir con su exponente.
+			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '^' || s == '(' || s == ')'){		// Si es un operador.
 				setToken(token, "Real");
 				setOperation(s);
 				resetState();
-			} else if (s == '/'){
+			} else if (s == '/'){				// Si es division o comentario.
 				setToken(token, "Real");
 				setState(9, s);
-			} else if (s == ' '){
+			} else if (s == ' '){				// Fin del token.
 				setToken(token, "Real");
 				resetState();
-			} else if (isdigit(s)){
+			} else if (isdigit(s)){				// Si es un real.
 				setState(5, s);
-			} else if (s == 'e' || s == 'E'){
+			} else if (s == 'e' || s == 'E'){	// Si es un real con exponente.
 				setState(6, s);
-			}  else {
+			}  else {							// Ninguna de las anteriores.
 				resetState();
 			}
 			break;
-		case 6:
-			if (s == '-'){
+		case 6:		// En el caso 6, se ha agregado el exponente 'e' o 'E', por lo que es necesario determinar el exponente positivo o negativo.
+			if (s == '-'){				// Si es un exponente negativo.
 				setState(7, s);
-			} else if (isdigit(s)) {
+			} else if (isdigit(s)) {	// Si es un exponente positivo.
 				setState(8, s);
-			} else {
+			} else {					// Ninguna de las anteriores.
 				resetState();
 			}
 			break;
-		case 7:
-			if (isdigit(s)){
+		case 7:		// En el caso 7, solo se puede agregar el ultimo digito del numero real que es el exponente.
+			if (isdigit(s)){		// Si es un exponente positivo.
 				setState(8, s);
-			} else {
+			} else {				// En este estado, como solo puede seguir siendo un real si se ingrea un solo digito, de no ser asi, se reinicia el token y el estado.
 				resetState();
 			}
 			break;
-		case 8:
-			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '^' || s == '(' || s == ')'){
-				setToken(token, "Real");
+		case 8:		// En el caso 8, es el fin del numero real por lo que podemos agregarlo a nuestro vector, solo queda evaluar el siguiente token o si es un real valido.
+			if (s == '=' || s == '+' || s == '-' || s == '*' || s == '^' || s == '(' || s == ')'){		// Si es un operador.
+				setToken(token, "Real");	
 				setOperation(s);
 				resetState();
-			} else if (s == '/'){
+			} else if (s == '/'){		// Si es division o comentario.
 				setToken(token, "Real");
 				setState(9, s);
-			} else {
+			} else {					// Si se reconoce otro caracter que no sea un operador, un caracter especial o comentario, entonces tenemos un real no valido.
 				resetState();
 			}
 			break;
-		case 9:
-			if ((s == '(' || s == ')')){
+		case 9:		// En el caso 9, tenemos que determinar si nuestro token = "/" es una division o comentario.
+			if ((s == '(' || s == ')')){			// Si es un caracter especial.
 				setOperation('/');
 				setOperation(s);
 				resetState();
-			} else if (s == ' '){
+			} else if (s == ' '){					// Si hay un espacio, es una division.
 				setOperation('/');
 				resetState();
-			} else if (s == '/' && token == "/"){
+			} else if (s == '/' && token == "/"){	// Si s = "/" y el token = "/", es una division.
 				setState(10, s);
-			} else {
+			} else {								// Ninguna de las anteriores.
 				resetState();
 			}
 			break;
-		case 10:
+		case 10:	// En el caso 10, sabemos que es un comentario, por lo que agregaremos cada caracter hasta terminar la linea de texto.
 			setState(10, s);
 			break;
-		default:
+		default:	// Ninguno de los casos.
 			break;
 		}
 	}
-	if (token.length() > 0){
-		switch (qg){
-		case 1:
+	if (token.length() > 0){	// Si al finalizar la linea de texto, pudimos evaluar algo en nuestro token, asignamos lo que posiblemente pudo haber sido dicho token.
+		switch (qg){			// Como tambien cambiamos entre estados previamente de terminar la linea de texto, podemos determinar por los estados finales quye era el token.
+		case 1:					// En el caso 1, es el estado final para variables.
 			setToken(token, "Variable");
 			break;
-		case 3:
+		case 3:					// En el caso 3, es el estado final para enteros.
 			setToken(token, "Entero");
 			break;
-		case 5:
+		case 5:					// En el caso 5, es el estado final para reales (sin exponente).
 			setToken(token, "Real");
 			break;
-		case 8:
+		case 8:					// En el caso 8, es el estado final para reales (con exponente).
 			setToken(token, "Real");
 			break;
-		case 10:
+		case 10:				// En el caso 10, es el estado final para comentarios.
 			setToken(token, "Comentario");
 			break;
-		default:
+		default:				// Si no es ninguno de los estados anteriores, nuestro token no es valido y no lo agregamos a nuestro vector.
 			resetState();
 			break;
 		}
 	}
-	resetState();
+	resetState();	// Reiniciamos el token y el estado por cada linea de texto.
 }
 
+// Funcion principal.
 int main(){
-	std::string str;
-	std::ifstream inputFile;
-	inputFile.open("input.txt");
-	if (!inputFile){
-		std::cout << "No se pueede abrir el archivo input.txt";
+	std::string str;				// String para cada linea del texto input.txt
+	std::ifstream inputFile;		// Ifstream para nuestro archivo de entrada.
+	inputFile.open("input.txt");	// Abrimos el archivo input.txt
+	if (!inputFile){				// Intentamos abrir el archivo, de no ser posible se sale del programa.
+		std::cout << "No se puede abrir el archivo input.txt";
 		exit(1);
 	}
-	while (getline(inputFile, str)){
-		// bool com = isComment(str);
-		//sVariable(str);
-		// isOperation(str);
-		//isReal(str);
-		//isInteger(str);
+	while (getline(inputFile, str)){	// Ciclo while para mandar cada linea del texto input.txt a la funcion DFA como parametro.
 		DFA(str);
 	}
-	for (int i = 0; i < vec.size(); i = i + 2){
+	for (int i = 0; i < vec.size(); i = i + 2){		// Ciclo for para imprimir el resultado de salida.
 		std::cout << vec[i] << "\t" << vec[i + 1] << std::endl;
 	}
-
-	inputFile.close();
-	return 0;
+	inputFile.close();	// Cerramos el archivo input.txt
+	return 0;			// Fin de la funcion principal.
 }
